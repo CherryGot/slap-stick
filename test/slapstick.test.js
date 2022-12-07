f06f2d159b9c963646b8bf7747e987a09f4fc874
Chakrapani Gautam <cherrygot.dev@gmail.com>
2022-12-07 19:37:11 +0530

    Wrote tests for exposed SlapStick API

--- test/slapstick.test.js
+++ test/slapstick.test.js
@@ -6,15 +6,71 @@
  * @jest-environment jsdom
  */
 
-import SlapStick from "../src/slapstick";
+import SlapStick from '../src/slapstick';
 
 const testSuit = 'Testing SlapStick class.';
 describe( testSuit, () => {
   let testName = 'Empty constructor call should throw a type error.';
   test( testName, testEmptyConstructorCall );
+
+  testName = 'Invalid selectors should throw error during initialization.';
+  test( testName, testInvalidSelectorInConstructor );
+
+  testName = 'Should throw error when html element is not found.';
+  test( testName, testNonExistentDOMElement );
+
+  testName = 'Html element should be positioned sticky on successful initalization.';
+  test( testName, testSuccessfulInitialization );
+
+  testName = 'Html element should be positioned back to what it was after destruction.';
+  test( testName, testSuccessfulDestruction );
 } );
 
 function testEmptyConstructorCall() {
-  expect(() => new SlapStick()).toThrowError(TypeError);
+  expect( () => new SlapStick() ).toThrowError( TypeError );
 }
 
+function testInvalidSelectorInConstructor() {
+  getSomeInvalidSelectors()
+    .forEach(
+      selector => {
+        expect( () => new SlapStick( selector ) ).toThrowError( TypeError );
+      }
+    );
+}
+
+function getSomeInvalidSelectors() {
+  return [
+    23,
+    {},
+    { 'a': 'b' },
+    [],
+    [1, 3, 5],
+    '',
+    false,
+  ];
+}
+
+function testNonExistentDOMElement() {
+  document.body.innerHTML = `<div></div>`;
+  expect( () => new SlapStick( '#sidebar' ) ).toThrowError( ReferenceError );
+}
+
+function testSuccessfulInitialization() {
+  document.body.innerHTML = `<div class="sticky-sidebar"></div>`;
+  const stickySidebar = new SlapStick( '.sticky-sidebar' );
+
+  expect( stickySidebar instanceof SlapStick ).toBe( true );
+  expect( document.querySelector( '.sticky-sidebar' ).style.position ).toBe( 'sticky' );
+}
+
+function testSuccessfulDestruction() {
+  document.body.innerHTML = `<div class="sticky-sidebar"></div>`;
+  const sidebar = document.querySelector( '.sticky-sidebar' );
+  const position = sidebar.style.position;
+
+  const stickySidebar = new SlapStick( '.sticky-sidebar' );
+  stickySidebar.destroy();
+
+  expect( sidebar.style.position ).toBe( position );
+}
